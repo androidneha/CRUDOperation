64c70a13918e810fa4e2da16df8c80f9b0021068
Neha <nehaappdev@gmail.com>
2017-02-26 21:07:07 +0530

    My First commit

---
+++ handler.php
@@ -0,0 +1,33 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    if (isset($_POST['name']) || isset($_POST['email']) || isset($_POST['contact']) || isset($_POST['pwd'])) 
+    {
+        if (!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['contact']) || !empty($_POST['pwd'])) 
+        {
+            $name=$_POST['name'];
+            $email=$_POST['email']; 
+            $contact=$_POST['contact'];
+            $pwd=$_POST['pwd'];
+            $con= mysqli_connect('localhost', 'root', '', 'CRUD');
+            $result= mysqli_query($con,"insert into user set user_name='$name',user_email='$email',user_contact='$contact',user_password='$pwd'");
+            $query="select user_id from user where user_email='$email' and user_password='$pwd'";
+            $result=mysqli_query($con, $query);
+            if(mysqli_num_rows($result)>0)
+            {
+                while($row= mysqli_fetch_array($result))
+                {
+                    echo $row['user_id'].".SUCCESS";
+                }
+            }
+        }
+        else 
+        {
+            echo 'Some Fields are Empty';
+        }
+    } 
+    else 
+    {
+        echo 'Some Fields are not Set';
+    }
+}
---
+++ view_modal.php
@@ -0,0 +1,26 @@
+<div id="load_popup_modal_contant" class="" role="dialog">
+<div class="modal-dialog modal-md">
+<?php
+?>
+	<form role="form" class="form-inline" role="form" id="form_load_content_id">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Welcome</h4>
+      </div>
+	    <div id="validation-error"></div>
+  <div class="cl"></div>
+	    <div class="modal-body">
+	<h3> Modal Popup content Here</h3>
+      </div>
+      <div class="modal-footer">
+
+      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	  </form>
+      </div>
+    </div>
+</div>
+</div>
+
+
+
